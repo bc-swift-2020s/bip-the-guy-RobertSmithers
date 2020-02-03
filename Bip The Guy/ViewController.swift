3d0014acc35164bbdeb8326ab2c3b8d0256f7d4c
RobertSmithers <rjsmithers3@gmail.com>
2020-02-03 16:23:20 -0500

    Completed 4.3

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     // Properties
     @IBOutlet weak var imageToPunch: UIImageView!
     
-    
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     // Functions
@@ -30,10 +36,22 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
     }
     
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: Data from \(soundName) could not be played as a sound")
+            }
+        } else {
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
     
     // IBActions
     @IBAction func libraryPressed(_ sender: UIButton) {
-
+       
     }
     
     @IBAction func cameraPressed(_ sender: UIButton) {
@@ -42,6 +60,7 @@ class ViewController: UIViewController {
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
     
 }
