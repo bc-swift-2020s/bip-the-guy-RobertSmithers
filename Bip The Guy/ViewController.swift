4901ab04b84d405d7b38b3d01939047458c23750
RobertSmithers <rjsmithers3@gmail.com>
2020-02-03 15:53:37 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by RJ Smithers on 2/3/20.
+//  Copyright © 2020 RJ Smithers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
